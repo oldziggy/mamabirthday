d7d18e8e2f87073ae09b27971c49f04640e7a838
Bogdan <bgdnvlkvb7@gmail.com>
2025-07-22 23:58:24 +0300

    support video and audio in models

--- wishes/models.py
+++ wishes/models.py
@@ -4,13 +4,17 @@ from django.db import models
 
 class Wish(models.Model):
 
-    wish_text = models.TextField()
+    wish_text = models.TextField(blank=True,null=True,default=None)
 
     sender = models.CharField(max_length=200)
 
     pub_date = models.DateTimeField('date published')
 
     image = models.ImageField(blank=True,null=True,default=None)
+
+    audio = models.FileField(blank=True, null=True,default=None)
+
+    video = models.FileField(blank=True, null=True,default=None)
 
 
     def __str__(self):
